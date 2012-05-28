caa9305ae1e3a01cbd13f7aa806bb420f503464b
William Jon McCann <jmccann@redhat.com>
2012-05-28 07:44:47 -0400

    Improve date display
    https://bugzilla.gnome.org/show_bug.cgi?id=676898

--- libnemo-private/nemo-file.h
+++ libnemo-private/nemo-file.h
@@ -397,11 +397,6 @@ char *                  nemo_file_get_string_attribute_with_default (NemoFile   
 									 const char                     *attribute_name);
 char *                  nemo_file_get_string_attribute_with_default_q (NemoFile                  *file,
 									 GQuark                          attribute_q);
-char *			nemo_file_fit_modified_date_as_string	(NemoFile 			*file,
-									 int				 width,
-									 NemoWidthMeasureCallback    measure_callback,
-									 NemoTruncateCallback	 truncate_callback,
-									 void				*measure_truncate_context);
 
 /* Matching with another URI. */
 gboolean                nemo_file_matches_uri                       (NemoFile                   *file,
@@ -496,8 +491,6 @@ void                    nemo_file_list_call_when_ready              (GList      
 									 NemoFileListCallback        callback,
 									 gpointer                        callback_data);
 void                    nemo_file_list_cancel_call_when_ready       (NemoFileListHandle         *handle);
-
-char *   nemo_file_get_date_as_string             (NemoFile *file, NemoDateType date_type);
 
 gchar *  nemo_file_construct_tooltip              (NemoFile *file, NemoFileTooltipFlags flags);
 
