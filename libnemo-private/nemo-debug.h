cd849b324c764f9a7d36b9983f041f4b9eb2de9f
Daniel Schürmann <daschuer@gmx.de>
2014-01-05 21:13:45 +0100

    Merge commit '2156d246e45933d424decd447203e39c26482da7' into nemo_master_merge
    Conflicts:
    	libnemo-private/Makefile.am
    	libnemo-private/nemo-file.c
    	libnemo-private/nemo-file.h
    	libnemo-private/nemo-search-directory.c
    	libnemo-private/nemo-search-engine-simple.c
    	libnemo-private/nemo-search-engine-tracker.c
    	libnemo-private/nemo-search-provider.h

--- libnemo-private/nemo-debug.h
+++ libnemo-private/nemo-debug.h
@@ -50,6 +50,7 @@ typedef enum {
   NEMO_DEBUG_ACTIONS = 1 << 15,
   NEMO_DEBUG_DESKTOP = 1 << 16,
   NEMO_DEBUG_SEARCH = 1 << 17,
+  NEMO_DEBUG_SEARCH_HIT = 1 << 18
 } DebugFlags;
 
 void nemo_debug_set_flags (DebugFlags flags);
