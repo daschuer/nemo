9c9594e1947c423c969866f34e1c698857cbd30e
Daniel Schürmann <daschuer@gmx.de>
2014-02-23 20:38:22 +0100

    Merge commit 'c68654c1a018faf5e55dee71c45617cd8e248855' into nemo_master_merge
    Conflicts:
    	libnemo-private/nemo-file.c
    	libnemo-private/nemo-file.h

--- libnemo-private/nemo-file.c
+++ libnemo-private/nemo-file.c
@@ -4366,6 +4366,12 @@ get_throttle_count (NemoFile *file)
     return details->thumbnail_throttle_count;
 }
 
+char *
+nemo_file_get_thumbnail_path (NemoFile *file)
+{
+	return g_strdup (file->details->thumbnail_path);
+}
+
 NemoIconInfo *
 nemo_file_get_icon (NemoFile *file,
 			int size,
